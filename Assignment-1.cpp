0e51f3eb74aa49c5efb8078acd494fb63233d10f
ruchika ruchika <ruchikaofficial02@gmail.com>
2025-03-16 16:29:36 +0530

    first commit

---
+++ Assignment-1.cpp
@@ -0,0 +1,27 @@
+// 1. Write a program that takes two integers as input and prints their sum. 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,sum;
+    cout<<"a = Enter your first number: ";
+    cin>>a;
+    cout<<"b = Enter your second number: ";
+    cin>>b;
+    sum = a+b;
+    cout<<"sum: "<<sum<<endl;
+    return 0;
+}
+
+//2.Take length and breadth as input (both integers) and calculate the area of the rectangle. 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int l,w,Area;
+    cout<<"l = Enter the lengthr: ";
+    cin>>l;
+    cout<<"w = Enter the width: ";
+    cin>>w;
+    Area = l*w;
+    cout<<"Area of rectangle: "<<Area<<endl;
+    return 0;
+}
